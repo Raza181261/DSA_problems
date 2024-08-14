94c206c5a06dde7ffe824a340c35c334a36319b7
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2024-08-14 22:47:13 +0400

    Time: 67 ms (50.61%) | Memory: 112.7 MB (21.31%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,34 +1,45 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>result;
+      vector<vector<int>> result;
+
+      void twoSum(vector<int>nums, int target, int i, int j){
+        while(i<j){
+            if(nums[i] + nums[j] > target)
+            j--;
+            else if(nums[i] + nums[j] < target)
+            i++;
+            else{
+                 while(i<j && nums[i] == nums[i+1]) i++;
+                 while(i<j && nums[j] == nums[j-1]) j--;
+
+                 result.push_back({-target, nums[i], nums[j]});
+
+                 i++;
+                 j--;
+
+
+            }
+        }
+      }
+
+    vector<vector<int>> threeSum(vector<int>& nums) { 
         int n = nums.size();
+        if(n<3){
+            return {};
+        }
+        result.clear();
 
         sort(nums.begin(), nums.end());
 
-        for(int i = 0; i<n; i++){
-           if(i>0 && nums[i] == nums[i-1])
-           continue;
-       
+        for (int i = 0; i < n; i++) {
+            if (i>0 && nums[i] == nums[i - 1])
+                continue;
+        
+        int n1 = nums[i];
+        int target = -n1;
 
-        unordered_map<int, int>H_map;
-        int target = -nums[i];
-
-        for(int j = i+1; j<n; j++){
-           int complement = target - nums[j];
-
-           if(H_map.find(complement) != H_map.end()){
-            result.push_back({nums[i], nums[j], complement});
-
-            while(j+1<n && nums[j] == nums[j+1]){
-                j++;
-
-            }
-           }
-           H_map[nums[j]] == j;
+        twoSum(nums, target, i+1, n-1);
         }
+        return result;
     }
-return result;
-    }
-    
 };
