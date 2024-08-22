28c8556a6013798761889eed42e852902c9dae38
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2024-08-22 08:51:43 +0400

    Time: 0 ms (100.00%) | Memory: 7.6 MB (54.04%) - LeetSync

--- 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
+++ 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     double angleClock(int hour, int minutes) {
         // positoin of hours
-        float h = (hour % 12 + (float)minutes/60)*30;  //360/12=30
+        float h = (hour % 12 + (float)minutes/60)*30;  //360/12=30 angle for each hour
 
         // position of minutes
-        float m = minutes * 6;
+        float m = minutes * 6;   // 360/60 angle for each minutes is 6
 
         float angle = abs(h-m);
 
