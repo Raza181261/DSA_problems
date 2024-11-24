7983e5c236d532c69a1a31eaef3f45d55a4eadce
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2024-11-24 23:22:40 +0500

    Time: 4 ms (45.07%) | Memory: 14.6 MB (49.57%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.cpp
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
-        int colSize = strs[0].size();
-        int rowSize = strs.size();
+        int colNum = strs[0].size();
+        int rowNum = strs.size();
         int delCount = 0;
 
-        for(int col = 0; col<colSize; col++){
-            for(int row = 0; row<rowSize-1; row++){
+        for(int col = 0; col<colNum; col++){
+            for(int row = 0; row<rowNum-1; row++){
                 if(strs[row][col] > strs[row+1][col]){
                 delCount++;
                 break;
