a3610aae5fbd437e83d1394df623f8c4bf80c562
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-02-17 21:57:23 +0500

    Time: 0 ms (100.00%) | Memory: 8 MB (47.31%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -4,7 +4,7 @@ public:
         if (n == 1) {
             return '0';
         }
-        int length = (1 << n) - 1; // pow(2,n)-1
+        int length = pow(2,n) - 1; // pow(2,n)-1
 
         if (k < ceil(length / 2.0)) {
             return findKthBit(n - 1, k);
