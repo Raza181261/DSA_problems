f137bba4422e1917c84a20f11e2637e4b851a9b8
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-03-10 22:16:00 +0500

    Time: 7 ms (5.10%) | Memory: 10.1 MB (7.48%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int n = s.length();
+        unordered_set<char>st;
+        int result = 0;
+
+        for(char &c:s){
+            if(st.count(c)){
+                result+=2;
+                st.erase(c);
+            }else{
+                st.insert(c); 
+            }
+        }
+        if(!st.empty()){
+            result++;
+        }
+        return result;
+    }
+};
