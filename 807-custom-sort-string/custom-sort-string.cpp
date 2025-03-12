e988c80709b1d6419df663ed83834e46c04aa7f9
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-03-12 22:38:51 +0500

    Time: 0 ms (100.00%) | Memory: 10.3 MB (5.07%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        int count[26] = {0};
+       vector<int> index(26,-1);
 
-        for(char &ch: s){
-            count[ch-'a']++;
-        }
-        string result = "";
-
-        for(char &ch : order){
-            while(count[ch-'a'] > 0){
-                result.push_back(ch);
-                count[ch-'a']--;
+       for(int i = 0; i<order.length(); i++){
+        char ch = order[i];
+        index[ch-'a'] = i;
+       }
+       
+        auto myComparater = [index](char& ch1, char& ch2){
+            if(index[ch1-'a']<index[ch2-'a']){
+                return true;
             }
-        }
-
-        for(char &ch : s){
-            if(count[ch-'a']>0){
-                result.push_back(ch);
-            }
-        }
-        return result;
+            return false;
+        };
+        
+        sort(begin(s),end(s),myComparater);
+        return s;
     }
 };
