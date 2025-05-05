e9a795b98ff2478149564b6f0100a642dd67b1ca
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-05-05 22:50:28 +0500

    Time: 19 ms (91.99%) | Memory: 70.9 MB (77.57%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        // sort(nums.begin(), nums.end());
-        // for(int i = 0; i<nums.size()-1; i++){
-        //     if(nums[i] == nums[i+1]){
-        //         return true;
+       sort(begin(nums), end(nums));
+       for(int i = 0; i<nums.size()-1; i++){
+        if(nums[i] == nums[i+1]){
+            return true;
+        }
+       }
+        return false;
+
+        // unordered_map<int,int>count;
+        // for(int num:nums){
+        //     if(count[num] > 0){
+        //          return true;
         //     }
+        //     count[num]++;
         // }
         // return false;
-
-        unordered_map<int,int>count;
-        for(int num:nums){
-            if(count[num] > 0){
-                 return true;
-            }
-            count[num]++;
-        }
-        return false;
     }
 };
