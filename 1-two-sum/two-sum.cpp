1e4a10d60ffd45356c4e6e9bd33ae806518776c5
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-05-06 15:43:12 +0500

    Time: 3 ms (72.41%) | Memory: 14.9 MB (36.41%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    //    unordered_map<int, int> num_map; // map to store (element, index) pairs
+        int n = nums.size();
 
-    //     for (int i = 0; i < nums.size(); ++i) {
-    //         int complement = target - nums[i];
-    //         if (num_map.find(complement) != num_map.end()) {
-    //             // If the complement exists in the map, return the pair of indices
-    //             return { num_map[complement], i };
-    //         }
-    //         // Add the current number to the map with its index
-    //         num_map[nums[i]] = i;
-    //     }
-
-    //     // If no solution is found, return an empty vector
-    //     return {};
-    // }
-
-    unordered_map <int, int> mp;
-    for(int i =0; i<nums.size(); i++){
-        int subtraction  = target - nums[i];
-        if(mp.find(subtraction) != mp.end()){
-            return {mp[subtraction], i};
+        unordered_map<int,int> mp;
+        for(int i = 0; i<n; i++){
+            int remaining = target - nums[i];
+            if(mp.find(remaining) != mp.end()){
+                return {mp[remaining],i};
+            }
+            mp[nums[i]] = i;
         }
-        mp[nums[i]] = i ;
+        return {};
     }
-    return {};
-}
 };
