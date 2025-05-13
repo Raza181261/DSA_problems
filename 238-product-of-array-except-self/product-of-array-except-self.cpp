2292c0f61b59103612aa83304b9a491751cb72e3
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-05-13 10:17:44 +0500

    Time: 0 ms (100.00%) | Memory: 40.1 MB (94.40%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -8,16 +8,15 @@ public:
         result[0] = 1;
 
         for(int i = 1; i<n; i++){
-            result[i] = result[i-1] * nums[i-1];
+            result[i] = result[i-1]* nums[i-1];
         }
 
         int right_product = 1;
 
         for(int i = n-1; i>=0; i--){
-         result[i] *= right_product;
-         right_product *= nums[i]; 
+            result[i] *= right_product;
+            right_product *= nums[i];
         }
-
         return result;
     }
 };
