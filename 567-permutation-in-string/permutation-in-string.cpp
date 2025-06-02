3700a6e0b60953b5c34506989f8ce97ef241c44c
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-06-02 15:12:59 +0500

    Time: 1 ms (75.18%) | Memory: 10 MB (46.87%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -8,14 +8,27 @@ public:
             return false; 
         }
 
-        sort(begin(s1),end(s1));
+        vector<int> s1_freq(26,0);
+        vector<int> s2_freq(26,0);
 
-        for(int i = 0; i<=m-n; i++){
-            string subString = s2.substr(i,n);
-            sort(begin(subString),end(subString));
-            if(s1 == subString){
+        for(char &ch:s1){
+            s1_freq[ch-'a']++;
+        }
+
+        int i = 0;
+        int j = 0;
+
+        while(j<m){
+            s2_freq[s2[j] -'a']++;
+
+            if(j-i+1 > n){
+                s2_freq[s2[i]-'a']--;
+                i++;
+            }
+            if(s1_freq == s2_freq){
                 return true;
             }
+            j++;
         }
         return false;
     }
