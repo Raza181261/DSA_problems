0f33e2604da31339b7cd593a867ee70d6508b9b2
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-06-19 09:45:13 +0500

    Time: 7 ms (68.48%) | Memory: 11.2 MB (94.75%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,45 +1,46 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n = s.length();
+        int n = s.size();
+        int m = t.size();
 
-        if(t.length() > n)
-         return "";
+        if (n < m) {
+            return "";
+        }
+        unordered_map<char, int> mp;
+        for (char ch : t) {
+            mp[ch]++;
+        }
 
-         unordered_map <char, int>mp;
-         //for storing elements in map
-         for(char &ch : t)
-         mp[ch]++;
+        int requiredCount = m;
+        int windowSize = INT_MAX;
+        int start_i = 0;
 
-         int windowSize = INT_MAX;
-         int required_count = t.length();
-         int i = 0,j = 0;
-         int str_i = 0;
+        int i = 0;
+        int j = 0;
 
-         //start jounrny to iterate
-         while(j < n){
+        while (j < n) {
             char ch = s[j];
             if (mp[ch] > 0)
-               required_count--;
-               mp[ch]--;
-            
-            while(required_count == 0){
-                //start srink
-                int currWindowSize = j-i+1;
-                 if(windowSize > currWindowSize){
-                    windowSize = currWindowSize;
-                    str_i = i;
-                 }
-                 mp[s[i]]++;
-                 if(mp[s[i]] > 0){
-                    required_count++;
-                 }
-                 i++;
-            }
-            j++;
-         }
-       
-       return windowSize == INT_MAX ? "" : s.substr(str_i, windowSize);
+                requiredCount--;
+            mp[ch]--;
 
+            while (requiredCount == 0) {
+                int currWindowSize = j - i + 1;
+
+                if (windowSize > currWindowSize) {
+                    windowSize = currWindowSize;
+                    start_i = i;
+                }
+                mp[s[i]]++;
+                if (mp[s[i]] > 0) {
+                    requiredCount++;
+                }
+                i++;
+            }
+
+            j++;
+        }
+        return windowSize == INT_MAX ? "" : s.substr(start_i, windowSize);
     }
 };
