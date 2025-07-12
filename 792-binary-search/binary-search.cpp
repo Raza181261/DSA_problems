630ed0440fa25308674309eeb4b954c62c72d4bc
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-07-12 10:55:19 +0500

    Time: 0 ms (100.00%) | Memory: 31.3 MB (79.99%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -4,16 +4,16 @@ public:
         int n = nums.size();
         int l = 0;
         int r = n-1;
-
-        while(l<=r){
-            int mid = (l+r)/2;
-
-            if(nums[mid] > target){
-                r = mid-1;
-            } else if(nums[mid] < target){
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > target) {
+                r = mid - 1;
+               
+            } else if (nums[mid] < target) {
+                
                 l = mid + 1;
-            }else{
-                return mid;
+            } else {
+                 return mid;
             }
         }
         return -1;
