f71eab7473529881b65fa05b0b798a3813fe8009
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-07-14 02:01:36 +0500

    Time: 0 ms (100.00%) | Memory: 95.8 MB (30.01%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& v1, vector<int>& v2) {
-        int c = v1.size() + v2.size();
-        vector<int> merged(c);
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 
-        // Merge the vectors using the merge function
-        merge(v1.begin(), v1.end(), v2.begin(), v2.end(), merged.begin());
+        
+        int m = nums1.size();
+        int n = nums2.size();
 
-        if (c % 2 == 0) {
-            // If the combined size is even, return the average of the two middle elements
-            return (merged[c/2 - 1] + merged[c/2]) / 2.0;
-        } else {
-            // If the combined size is odd, return the middle element
-            return merged[c/2];
+        vector<int> temp(m+n);
+
+        int i = 0;
+        int j = 0;
+
+        int k = 0;
+
+        while(i<m && j<n){
+            if(nums1[i] < nums2[j]){
+                temp[k++] = nums1[i++];
+            } else {
+                temp[k++] = nums2[j++];
+            }
         }
+
+        while(i < m){
+            temp[k++] = nums1[i++];
+        }
+        while(
+            j < n){
+            temp[k++] = nums2[j++];
+        }
+
+        int size = m+n;
+
+        if(size%2 == 1){
+            return temp[size/2];
+        }
+        return (temp[size/2] + temp[(size/2)-1])/2.0;
+
+
     }
 };
