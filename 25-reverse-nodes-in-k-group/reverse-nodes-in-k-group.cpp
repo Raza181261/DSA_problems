26107e88230ec7605fc082d95f738b53c30c92b7
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-08-04 10:52:18 +0500

    Time: 0 ms (100.00%) | Memory: 16.5 MB (71.16%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -1,46 +1,34 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    int getlength(ListNode* head){
-        ListNode* temp=head;
-        int len=0;
-        while(temp!=NULL){
-            temp=temp->next;
-            len++;
-        }
-        return len;
-    }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(head==NULL || head->next==NULL){
-            return head;
+        ListNode* temp = head;
+        int count = 0;
+
+        // check if k node exits
+        while (count < k) {
+            if (temp == NULL) {
+                return head;
+            }
+            temp = temp->next;
+            count++;
         }
-        int len=getlength(head);
-        if(len<k){
-            return head;
+
+        // recursively call rest part of LinkedList
+        ListNode* prevNode = reverseKGroup(temp, k);
+
+            // reverse current part
+            temp = head;
+        count = 0;
+
+        while (count < k) {
+            ListNode* Next = temp->next;
+            temp->next = prevNode;
+            prevNode = temp;
+            temp = Next;
+
+            count++;
         }
-        int position=0;
-        ListNode* prev=NULL;
-        ListNode* curr=head;
-        while(position<k){
-            ListNode* forward=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=forward;
-            position++;
-        }
-        if(curr!=NULL){
-            ListNode* recursionhead=reverseKGroup(curr,k);
-            head->next=recursionhead;
-        }
-        return prev;
+
+        return prevNode;
     }
 };
