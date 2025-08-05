9078735634f888c7c102dc2904b9f92f0f2bff7d
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-08-05 10:12:19 +0500

    Time: 0 ms (100.00%) | Memory: 23.7 MB (72.07%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -6,31 +6,28 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-       int maxD = 0;
-       int height(TreeNode* node){
-        if(node == nullptr)
-        return 0;
+    int solve(TreeNode* root, int& result) {
+        if (root == NULL)
+            return 0;
+        int left = solve(root->left, result);
+        int right = solve(root->right, result);
 
-        int leftHight = height(node -> left);
-        int rightHight = height(node -> right);
-
-        maxD = max(maxD, leftHight + rightHight);
-
-        return max(leftHight, rightHight)+1;
-
-        
-
-
-       }
+        result =  max(result, left + right);
+        return max(left, right) + 1;
+    }
     int diameterOfBinaryTree(TreeNode* root) {
-        height(root);
-        return maxD;
+        if (root == NULL)
+            return 0;
 
-        
+        int result = INT_MIN;
+
+        solve(root, result);
+        return result;
     }
 };
