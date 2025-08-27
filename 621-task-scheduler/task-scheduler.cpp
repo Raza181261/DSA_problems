fb33fb18dc060548ccbfd1ad0f59db7d866f27aa
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-08-27 15:36:08 +0500

    Time: 46 ms (15.76%) | Memory: 47.6 MB (40.67%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        vector<int> mp(26, 0);
-
-        for(char &ch : tasks){
+        vector<int> mp(26,0);
+        for(char &ch:tasks){
             mp[ch-'A']++;
         }
-
         int time = 0;
 
-        priority_queue<int> pq;
-
-        for(int i=0; i<26; i++ ){
-        if(mp[i] > 0){
-            pq.push(mp[i]);
-        }
+        priority_queue<int> pq; // By default max-heap
+        for(int i = 0; i<26; i++){
+            if(mp[i] > 0){
+                pq.push(mp[i]);
+            }
         }
 
         while(!pq.empty()){
             vector<int> temp;
-
             for(int i = 1; i<=n+1; i++){
                 if(!pq.empty()){
                     int freq = pq.top();
@@ -29,18 +25,19 @@ public:
                 }
             }
 
-            for(int &f: temp){
-                if(f > 0){
+            for(int f:temp){
+                if(f>0){
                     pq.push(f);
                 }
             }
 
             if(pq.empty()){
                 time += temp.size();
-            }else{
+            } else {
                 time += n+1;
             }
         }
+
         return time;
     }
 };
