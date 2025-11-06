0eb9ec59b8e31350127d197ee17a603d9b758d77
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-11-06 10:55:32 +0500

    Time: 27 ms (33.09%) | Memory: 11.2 MB (46.14%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-   int t[1001][1001];
-    bool check(string &s, int i, int j){
-        if(i > j){
-            return true;
-        }
-        if(t[i][j] != -1){
-            return t[i][j];
-        }
-        if(s[i] == s[j])
-        return t[i][j] = check(s, i+1, j-1);
-
-        return t[i][j] = false;
-    }
     int countSubstrings(string s) {
         int n = s.length();
-        memset(t,-1,sizeof(t));
-        //t[i][j] == -1 (not solved yet)
-        //t[i][j] == 0 (false)
-        //t[i][j] == 1 (true)
 
+        
+
+        vector<vector<bool>> t(n,vector<bool>(n,false));
         int count = 0;
 
-        for(int i = 0; i < n; i++){
-            for(int j = i ; j < n; j++){
-                if(check(s,i,j)){
+        for(int L = 1; L <= n; L++){
+            for(int i = 0; i+L-1 < n; i++){
+                int j = i+L-1;
+
+                if(i == j){ //length is 1
+                    t[i][j] = true;
+                } else if(i+1 == j){ // len is 2
+                    t[i][j] = (s[i] == s[j]);
+                } else { // len is more then 2
+                    t[i][j] = (s[i] == s[j] && t[i+1][j-1]);
+                }
+                if(t[i][j]){
                     count++;
                 }
+                
             }
         }
         return count;
