7c107a22bb38e2121bed9c17e86b435e5a94ac59
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-11-17 15:52:52 +0500

    Time: 16 ms (51.27%) | Memory: 26.2 MB (24.84%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -9,12 +9,9 @@ public:
                 return 0;
             }
         }
-        if(t[i][currSum+S] != -1){
-            return t[i][currSum+S];
+        if(t[i][currSum+S] != -1){ // jo answers -ve ma aa rhy hai oss ko positive krne k liye
+            return t[i][currSum+S]; // jo answers -ve ma aa rhy hai oss ko positive krne k liye
         }
-
-       
-
         int plus  = solve(nums,target, i+1, currSum + nums[i],t);
         int minus = solve(nums,target, i+1, currSum - nums[i], t);
 
@@ -23,7 +20,7 @@ public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         S = accumulate(begin(nums), end(nums) ,0);
-        vector<vector<int>> t(n+1, vector<int>(2*S+1, -1));
+        vector<vector<int>> t(n+1, vector<int>(2*S+1, -1)); // jo answers -ve ma aa rhy hai oss ko positive krne k liye
 
         return solve(nums,target,0,0,t);
     }
