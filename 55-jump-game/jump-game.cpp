45e3e04529be4edd81dbfcbe62b7b5c658489fa0
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-11-19 10:41:42 +0500

    Time: 516 ms (10.39%) | Memory: 53.4 MB (12.12%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+     int n;
+     int t[10001];
+     bool solve(vector<int>& nums, int n, int idx){
+        if(idx == n-1)
+        return true;
+        if(idx >= n)
+        return false;
+
+        if(t[idx] != -1){
+            return t[idx];
+        }
+
+        for(int i = 1; i<=nums[idx]; i++){
+            if(solve(nums,n,idx+i) == true){
+                return t[idx] = true;
+            } 
+        }
+        return t[idx] = false;
+     }
+    bool canJump(vector<int>& nums) {
+        n = nums.size();
+        memset(t,-1,sizeof(t));
+        return solve(nums,n,0);
+    }
+};
