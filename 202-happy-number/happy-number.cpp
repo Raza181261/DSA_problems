7ff0fbb5235d2b70de6222fdd855c3fa434fe38d
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-12-09 11:39:28 +0500

    Time: 0 ms (100.00%) | Memory: 7.9 MB (70.97%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
-    bool isHappy(int n) 
-    {
-        int slow = n;
-        int fast = n;
 
-        while (true)
-        {
-            slow = solve(slow);
-            fast = solve(solve(fast));
-
-            if (fast == 1)
-            {
-                return true;
-            }
-
-            if (slow == fast)
-            {
-                return false;
-            }
-        }
-    }
-
-    int solve(int n)
-    {
+    int help(int n){
         int sum = 0;
-
-        while (n > 0)
-        {
-            int digit = n % 10;
-            sum += digit * digit;
-            n /= 10;
+        while(n){
+            int digit = n%10;
+            sum += digit*digit;
+            n/=10;
         }
-
         return sum;
+    }
+    bool isHappy(int n) {
+        int slow = n, fast = n;
+        do{
+            slow  = help(slow);
+            fast = help(help(fast));
+        }while(slow != fast);
+        return slow == 1;
     }
 };
