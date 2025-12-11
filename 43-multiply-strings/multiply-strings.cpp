72c4f7836e5614d5528eb13d92e2b78eeddd03a1
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-12-11 10:56:06 +0500

    Time: 0 ms (100.00%) | Memory: 9.4 MB (85.30%) - LeetSync

--- 43-multiply-strings/multiply-strings.cpp
+++ 43-multiply-strings/multiply-strings.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        int n = num1.size();
-        int m = num2.size();
-        vector<int>result(n+m,0);
+        int m = num1.size();
+        int n = num2.size();
 
-        for(int i = n-1; i>=0; i--){
-            for(int j = m-1; j>=0; j--){
-               int mul = (num1[i] - '0') * (num2[j] - '0');
-               int sum = mul + result[i+j+1];
-               result[i+j+1] = sum % 10;
-               result[i + j] += sum / 10;
+        vector<int> result(m+n,0);
+
+        for(int i = m-1; i>=0; i--){
+            for(int j = n-1; j>=0; j--){
+                int mul = (num1[i] - '0') * (num2[j] - '0'); // convert string to integer
+                int sum = mul + result[i+j+1];
+                result[i+j+1] = sum % 10;
+                result[i+j] += sum/10;
             }
         }
-        string resultStr;
-        for(int num: result){
-            if(!(resultStr.empty() && num == 0)){
-                resultStr.push_back(num + '0');
+
+        string final_res;
+        for(int num : result){
+            if(!(final_res.empty() && num == 0)){
+                final_res.push_back(num + '0'); // convert integer to string
             }
         }
-        return resultStr.empty() ? "0" : resultStr;
+        return final_res.empty() ? "0" : final_res;
     }
 };
