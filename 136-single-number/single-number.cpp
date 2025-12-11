10daea4fb18f6c5cb655ee6d62e1afd19e6486ba
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-12-11 11:41:17 +0500

    Time: 0 ms (100.00%) | Memory: 20.7 MB (58.05%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -3,7 +3,9 @@ public:
     int singleNumber(vector<int>& nums) {
         int ans  = nums[0];
         for(int i = 1; i<nums.size(); i++){
-            ans = ans ^ nums[i];
+            ans = ans ^ nums[i]; // 2 ^ 2
+        //  ans = 10 ^ 10 ==> 00
+        //  ans  = 0
         }
         return ans;
     }
