19789279d984faa9e859f4db9708f7bb2814b87f
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-12-15 13:05:28 +0500

    Time: 0 ms (100.00%) | Memory: 8.3 MB (12.64%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        int count = 0;
-       while(n){
-        count += (n%2); //this gives reminder
-        n /= 2; // this gives simple divide 
-       }
-        return count;
+        return __builtin_popcount(n);
     }
 };
