e357acbbe0a86ebb36c0a7f4c08f0f7b420135ec
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-12-15 12:51:07 +0500

    Time: 0 ms (100.00%) | Memory: 8.2 MB (80.63%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        //Approach 1 :
-        // just we use one preDefine funtion
-        //return __buildin_popcount(n)
-
-         //Approach 2 :  
         int count = 0;
-
-       while( n > 0){
-        n = (n & (n-1));
-        count++;
-       }
+        for(int i = 31; i>=0; i--){
+            if(((n>>i) & 1) == 1){
+                count++;
+            }
+        }
         return count;
     }
 };
