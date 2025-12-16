823d34f8618b647a06155a3c4dbac405b5447dd7
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-12-16 11:02:37 +0500

    Time: 0 ms (100.00%) | Memory: 10.7 MB (55.44%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> result(n+1);
-        for(int i = 0; i<n+1; i++){
-        result[i] = __builtin_popcount(i);
+        vector<int> result(n + 1);
+
+        if (n == 0)
+            return result;
+
+            result[0] = 0; // binary of 0 has 0 number of bits
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                result[i] = result[i / 2] + 1;
+            } else {
+                result[i] = result[i / 2];
+            }
         }
         return result;
-
     }
-}; 
+};
