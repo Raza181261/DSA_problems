fb912f0b4a394698020cc7dbdfab75d9a37ec150
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-12-22 11:03:40 +0500

    Time: 9 ms (13.36%) | Memory: 21.7 MB (92.35%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -7,7 +7,8 @@ public:
         int l = 0, r = n-1;
 
         while(l <= r){
-            int mid = l+(r-l)/2;
+            // int mid = l+(r-l)/2;
+            int mid = r+l/2;
             if(nums[mid] > mid){
                 result = mid;
                 r = mid-1; // go left and explore kia koi element missing tuo nhi
