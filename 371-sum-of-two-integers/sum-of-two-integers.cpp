22d3d8c002b385dfd9d728f65fb72c26de09fb68
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-12-22 11:32:44 +0500

    Time: 0 ms (100.00%) | Memory: 7.8 MB (64.31%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        while(b != 0){
-            int temp = (a & b)<< 1;
-            a = a ^ b;
-            b = temp;
-        }
-        return a;
+        int sum;
+        return sum = a + b;
     }
 };
