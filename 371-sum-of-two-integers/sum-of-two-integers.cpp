27e08c5121462747f2b53a8a1915c572adea937c
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2025-12-22 11:51:40 +0500

    Time: 0 ms (100.00%) | Memory: 7.8 MB (64.31%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        int sum;
-        return sum = a + b;
+        while(b != 0){ // this is condition jab tk carry zero nhi hoo jta
+            int carry = (a & b) << 1; // carry calculate
+            a = a ^ b; // sum without using operator
+            b = carry; // carry ko next iteration k liye pass kr doo
+        }
+        return a;
     }
 };
