4f7190848e842fed92440c9215a5db587b0ab623
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2026-01-02 10:08:20 +0500

    Time: 7 ms (98.12%) | Memory: 60.2 MB (89.54%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> numSet(nums.begin(), nums.end());
-        int longest = 0;
+        // unordered_set<int> numSet(nums.begin(), nums.end());
+        // int longest = 0;
 
-        for(int num:numSet){
-            if(numSet.find(num-1) == numSet.end()){
-                int currNum = num;
-                int streak = 1;
+        // for(int num:numSet){
+        //     if(numSet.find(num-1) == numSet.end()){ // this line return true if number not present 
+        //         int currNum = num;
+        //         int streak = 1;
 
-                while(numSet.find(currNum+1) != numSet.end()){
-                    currNum += 1;
-                    streak += 1;
-                }
-                longest = max(longest,streak);
+        //         while(numSet.find(currNum+1) != numSet.end()){
+        //             currNum += 1;
+        //             streak += 1;
+        //         }
+        //         longest = max(longest,streak);
+        //     }
+
+        // }
+        // return longest;
+        
+         int currLength = 1;
+         int maxLength = 1;
+         int n = nums.size();
+         if(n == 0){
+            return 0;
+         } 
+         sort(begin(nums), end(nums));
+         for(int i = 1; i<n; i++){
+            if(nums[i] == nums[i-1]){ //it will check duplicates
+                continue;
+            }else if(nums[i] == nums[i-1]+1){ // it will check consecutive elements;
+                currLength += 1;
+            }else{
+                currLength = 1;
             }
 
-        }
-        return longest;
+            maxLength = max(currLength, maxLength);
+         }
+         return maxLength;
     }
 };
