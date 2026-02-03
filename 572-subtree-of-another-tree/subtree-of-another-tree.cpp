f7562c5f7a78b328d74c98c05d52be7ef3033112
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2026-02-03 10:24:55 +0500

    Time: 3 ms (28.58%) | Memory: 29.1 MB (39.40%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -6,24 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-     bool isSameTree(TreeNode* s, TreeNode* t) {
-        if (!s && !t) return true;
-        if (!s || !t) return false;
-        if (s->val != t->val) return false;
+    bool isSameTree(TreeNode* s, TreeNode* t) {
+        if (s == NULL && t == NULL)
+            return true;
+        if (s == NULL || t == NULL)
+            return false;
+        if (s->val != t->val)
+            return false;
         return isSameTree(s->left, t->left) && isSameTree(s->right, t->right);
     }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(root == NULL)
-        return false;
+        if (root == NULL)
+            return false;
 
-        if(isSameTree(root, subRoot))
-        return true;
+        if (isSameTree(root, subRoot))
+            return true;
 
-        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+        return isSubtree(root->left, subRoot) ||
+               isSubtree(root->right, subRoot);
+
     }
 };
