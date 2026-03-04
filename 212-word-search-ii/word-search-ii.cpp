230e9cec3b45bca2cef1a7c6756f6e90ebac47c8
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2026-03-04 14:08:10 +0500

    Time: 333 ms (56.44%) | Memory: 19 MB (77.71%) - LeetSync

--- 212-word-search-ii/word-search-ii.cpp
+++ 212-word-search-ii/word-search-ii.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<string> result;
     int m, n;
-    vector<vector<int>> direction{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     struct trieNode {
         bool endOfWord;
@@ -12,9 +11,9 @@ public:
 
     trieNode* getNode() {
         trieNode* newNode = new trieNode();
-
         newNode->endOfWord = false;
         newNode->word = "";
+
         for (int i = 0; i < 26; i++) {
             newNode->children[i] = NULL;
         }
@@ -23,43 +22,49 @@ public:
 
     void insert(trieNode* root, string& word) {
         trieNode* crawler = root;
+
         for (int i = 0; i < word.length(); i++) {
             char ch = word[i];
+            int idx = ch - 'a';
 
-            if (crawler->children[ch - 'a'] == NULL) {
-                crawler->children[ch - 'a'] = getNode();
+            if (crawler->children[idx] == NULL) {
+                crawler->children[idx] = getNode();
             }
-            crawler = crawler->children[ch - 'a'];
+            crawler = crawler->children[idx];
         }
         crawler->endOfWord = true;
         crawler->word = word;
     }
 
+    vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
     void findTrieWords(vector<vector<char>>& board, int i, int j,
                        trieNode* root) {
-        if (i < 0 || i >= m || j < 0 || j >= n)
+
+        if (i < 0 || j < 0 || i >= m || j >= n)
             return;
 
-        if (board[i][j] == '$' || root->children[board[i][j] - 'a'] == NULL)
+        char ch = board[i][j];
+        int idx = ch - 'a';
+
+        if (board[i][j] == '$' || root->children[idx] == NULL)
             return;
 
-        root = root->children[board[i][j] - 'a'];
-
+        root = root->children[idx];
         if (root->endOfWord == true) {
             result.push_back(root->word);
             root->endOfWord = false;
         }
-
         char temp = board[i][j];
-        board[i][j] = '$'; // mark visited
+        board[i][j] = '$'; // mark to '$'
 
-        for (vector<int>& dir : direction) {
+        for (auto& dir : directions) {
             int new_i = i + dir[0];
             int new_j = j + dir[1];
 
             findTrieWords(board, new_i, new_j, root);
         }
-        board[i][j] = temp; // mark Unvisited
+        board[i][j] = temp; // unmark from '$'
     }
 
     vector<string> findWords(vector<vector<char>>& board,
@@ -69,7 +74,6 @@ public:
 
         // create root
         trieNode* root = getNode();
-
         // insert word in trie
         for (string& word : words) {
             insert(root, word);
@@ -78,13 +82,13 @@ public:
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 char ch = board[i][j];
+                int idx = ch - 'a';
 
-                if (root->children[ch - 'a'] != NULL) {
+                if (root->children[idx] != NULL) {
                     findTrieWords(board, i, j, root);
                 }
             }
         }
-
         return result;
     }
 };
