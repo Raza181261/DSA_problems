0d2c29c47011e2c6a9b932edff6862a83423c96f
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2026-06-17 09:49:55 +0500

    Time: 0 ms (100.00%) | Memory: 9.8 MB (49.27%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        vector<int> count(26,0);
 
-        return t == s;
+        for(char &ch : s){
+            count[ch-'a']++;
+        }
+
+        for(char &ch :t){
+            count[ch-'a']--;
+        }
+
+        bool allZero = all_of(
+            begin(count),end(count),[](int element){
+                return element == 0;
+            }
+        );
+
+        return allZero;
     }
 };
