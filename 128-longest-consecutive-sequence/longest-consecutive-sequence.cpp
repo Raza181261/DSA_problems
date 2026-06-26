9dc8e46a89f75206bcd6963b4259bdc29b71c41d
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2026-06-26 22:31:41 +0500

    Time: 16 ms (87.54%) | Memory: 60.2 MB (92.69%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        // unordered_set<int> numSet(nums.begin(), nums.end());
-        // int longest = 0;
-
-        // for(int num:numSet){
-        //     if(numSet.find(num-1) == numSet.end()){ // this line return true if number not present 
-        //         int currNum = num;
-        //         int streak = 1;
-
-        //         while(numSet.find(currNum+1) != numSet.end()){
-        //             currNum += 1;
-        //             streak += 1;
-        //         }
-        //         longest = max(longest,streak);
-        //     }
-
-        // }
-        // return longest;
-        
-         int currLength = 1;
-         int maxLength = 1;
-         int n = nums.size();
-         if(n == 0){
+        int curr_length = 1;
+        int max_length = 1;
+        int n = nums.size();
+        if(n == 0){
             return 0;
-         } 
-         sort(begin(nums), end(nums));
-         for(int i = 1; i<n; i++){
-            if(nums[i] == nums[i-1]){ //it will check duplicates
+        }
+
+        sort(begin(nums), end(nums));
+
+        for(int i = 1; i < n; i++){
+            if(nums[i] == nums[i-1]){
                 continue;
-            }else if(nums[i] == nums[i-1]+1){ // it will check consecutive elements;
-                currLength += 1;
-            }else{
-                currLength = 1;
+            } else if(nums[i] == nums[i-1]+1){
+                curr_length += 1;
+            } else {
+                curr_length = 1;
             }
 
-            maxLength = max(currLength, maxLength);
-         }
-         return maxLength;
+            max_length = max(max_length, curr_length);
+        }
+
+        return max_length;
     }
 };
