4490ab979717cfb0a8c45118517683120c561ea1
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2026-06-28 00:35:53 +0500

    Time: 16 ms (87.41%) | Memory: 60.3 MB (85.16%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -3,17 +3,19 @@ public:
     int longestConsecutive(vector<int>& nums) {
         int curr_length = 1;
         int max_length = 1;
+
         int n = nums.size();
-        if (n == 0) {
-            return 0;
-        }
 
         sort(begin(nums), end(nums));
 
-        for (int i = 1; i < n; i++) {
-            if (nums[i] == nums[i - 1]) {
+        if(n == 0){
+            return 0;
+        }
+
+        for(int i = 1; i < n; i++){
+            if(nums[i] == nums[i-1]){
                 continue;
-            } else if (nums[i] == nums[i - 1] + 1) {
+            } else if(nums[i] == nums[i-1]+1){
                 curr_length += 1;
             } else {
                 curr_length = 1;
@@ -21,7 +23,6 @@ public:
 
             max_length = max(max_length, curr_length);
         }
-
         return max_length;
     }
 };
